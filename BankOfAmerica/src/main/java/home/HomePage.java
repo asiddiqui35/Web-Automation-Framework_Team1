9f0d90ed7a7ec6e5e89df0a9eb5036444691599a
Selina Tanha <tanha.selina75@gmail.com>
2018-04-22 21:31:51 -0400

    Trial commit

--- BankOfAmerica/src/main/java/home/HomePage.java
+++ BankOfAmerica/src/main/java/home/HomePage.java
@@ -6,7 +6,7 @@ import org.testng.Assert;
 public class HomePage extends CommonAPI {
     public void checkHomePage(){
         String title = driver.getTitle();
-        Assert.assertEquals(title, "Bank of America - Banking, Credit Cards, Home Loans and Auto Loans");
+        Assert.assertEquals(title, "Bank of America - Banking, Credit Cards, home Loans and Auto Loans");
         System.out.println("Unit Testing: passed");
     }
 }
