93c1e681f6a9dd8ffdea6f62b8791b068908f8a6
moynabegum <bmaya2013@gmail.com>
2018-04-24 17:46:00 -0400

    testing my branch

--- FaceBook/src/test/java/home/page/TestPageFB.java
+++ FaceBook/src/test/java/home/page/TestPageFB.java
@@ -1,5 +1,6 @@
 package home.page;
 
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -7,13 +8,15 @@ public class TestPageFB extends HomePageFB {
 
     @Test
     public void TestcheckBrowser() {
-
-     Assert.assertEquals( checkBrowser(),"Facebook - Log In or Sign Up");
-
+        Assert.assertEquals( checkBrowser(),"Facebook - Log In or Sign Up");
+        driver.findElement(By.xpath("//input[@class='inputtext' and @name='email']")).sendKeys("maya813r@yahoo.com");
+        driver.findElement(By.xpath("//*[@id=\'pass\']")).sendKeys("@mirtumi001");
+        driver.findElement(By.xpath("//*[@id=\"u_0_2\"]")).click();
+        }
+    @Test
+    public void NewsFeedLink() {
+        driver.findElement(By.xpath("//*[@id=\"navItem_217974574879787\"]/a/div")).click();
 
     }
-
-    /*public void CheckSignIn()*/
-
 
 }
