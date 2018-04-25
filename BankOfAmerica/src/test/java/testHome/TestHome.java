ada48ac767fe67f447df51e746be345bfa45f75f
ASiddiqui <aftabsiddiqui3@gmail.com>
2018-04-24 23:10:35 -0400

    some test cases

--- BankOfAmerica/src/test/java/testHome/TestHome.java
+++ BankOfAmerica/src/test/java/testHome/TestHome.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 public class TestHome extends HomePage{
     @Test
     public void testHome(){
+
         checkHomePage();
     }
 }
--- HBO/test/java/home/TestHome.java
+++ HBO/test/java/home/TestHome.java
@@ -5,10 +5,18 @@ import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
 
 public class TestHome extends Home{
-    @Test
-    public void testHome(){
-        homepage();
-        driver.findElement(By.xpath("/html/body/main/div[1]/div/div/header/div[1]/div/div[2]/div[2]/div[1]/input")).sendKeys("Game of Thrones", Keys.ENTER);;
-
+//    @Test //test1
+//    public void testHome(){
+//        clickSearch();
+//        putNames();
+//    }
+    @Test //test3
+    public void testHomeTab(){
+        chekHomeTabElements();
     }
+//    @Test //test4
+//    public void testPlayTrailer() {
+//        playTrailer();
+//
+//    }
 }
