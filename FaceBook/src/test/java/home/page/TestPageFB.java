1a1f5906b5181cd2ae14ccc0f25eee8c3e00f083
moynabegum <bmaya2013@gmail.com>
2018-04-25 16:09:55 -0400

    checking plugins

--- FaceBook/src/test/java/home/page/TestPageFB.java
+++ FaceBook/src/test/java/home/page/TestPageFB.java
@@ -8,11 +8,12 @@ public class TestPageFB extends HomePageFB {
 
     @Test
     public void testcheckBrowser() {
-        Assert.assertEquals( checkBrowser(),"Facebook - Log In or Sign Up");
+
         driver.findElement(By.xpath("//input[@class='inputtext' and @name='email']")).sendKeys("");
         driver.findElement(By.xpath("//*[@id=\'pass\']")).sendKeys("");
        // driver.findElement(By.xpath("//*[@id=\"u_0_2\"]")).click();
         driver.findElement(By.xpath("//*[@id=\"login_form\"]/table/tbody/tr[3]/td[2]/div/a")).click();
+        Assert.assertEquals( checkBrowser(),"Facebook - Log In or Sign Up");
         }
      public void Registration(){
         driver.findElement(By.cssSelector("//*[@id=\"u_0_o\"]")).sendKeys();
