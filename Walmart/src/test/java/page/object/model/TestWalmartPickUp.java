1e788642349005ebebc3f897d229842a5ac32a85
Selina Tanha <tanha.selina75@gmail.com>
2018-04-28 15:16:36 -0400

    testing.2

--- Walmart/src/test/java/page/object/model/TestWalmartPickUp.java
+++ Walmart/src/test/java/page/object/model/TestWalmartPickUp.java
@@ -6,7 +6,6 @@ import org.testng.annotations.Test;
 public class TestWalmartPickUp extends WalmartPickUp{
 
     @Test
-
     public void pickUp () throws InterruptedException {
         WalmartPickUp pickUp = PageFactory.initElements(driver, WalmartPickUp.class);
         System.out.println(driver.getTitle()+ " " + driver.getCurrentUrl());
