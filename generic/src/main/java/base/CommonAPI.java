b52a6949ef2dbfe4644ec0d0c96b95652ab557ea
ASiddiqui <aftabsiddiqui3@gmail.com>
2018-04-29 13:34:03 -0400

    rectified CommonAPI

--- generic/src/main/java/base/CommonAPI.java
+++ generic/src/main/java/base/CommonAPI.java
@@ -67,10 +67,6 @@ public class CommonAPI {
         ExtentTestManager.getTest().getTest().setStartedTime(getTime(result.getStartMillis()));
         ExtentTestManager.getTest().getTest().setEndedTime(getTime(result.getEndMillis()));
 
-        public void afterMethod() {driver.quit();
-    }
-
-    public void clickOnCss(String locator) {
         for (String group : result.getMethod().getGroups()) {
             ExtentTestManager.getTest().assignCategory(group);
         }
@@ -89,7 +85,6 @@ public class CommonAPI {
         }
         driver.quit();
     }
-
     @AfterSuite
     public void generateReport() {
         extent.close();
