b9d773045e75e4f0af978b10b315f6d58080898e
moynabegum <bmaya2013@gmail.com>
2018-05-01 04:10:12 -0400

    Progress

--- Fidelis/src/main/java/home/page/model/HomePage.java
+++ Fidelis/src/main/java/home/page/model/HomePage.java
@@ -7,8 +7,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends CommonAPI {
-
-
     @FindBy(id = "headerSearch")
     public WebElement SearchInput;
     @FindBy(css = "#dnn_ctr2267_HtmlModule_lblContent > div:nth-child(3) > div > em")
@@ -17,18 +15,38 @@ public class HomePage extends CommonAPI {
     public WebElement getSearchInput() {
         return SearchInput;
     }
-
     public void setSearchInput(WebElement searchInput) {
         this.SearchInput = searchInput;
     }
-
     public WebElement getSearchBtn() {
         return searchBtn;
     }
-
     public void setSearchBtn(WebElement searchBtn) {
         this.searchBtn = searchBtn;
     }
-
+    public void  clickPlan(WebDriver driver)throws InterruptedException {
+        getSearchBtn().click();
+        getSearchInput().clear();
+        getSearchInput().sendKeys("Plans", Keys.ENTER);
+        getSearchBtn().click();
+        Thread.sleep(2000);
+        driver.navigate().back();
+        Thread.sleep(2000);
+    }
+    public void searchDoctors(WebDriver driver)throws InterruptedException {
+        getSearchBtn().click();
+        getSearchInput().clear();
+        getSearchInput().sendKeys("Doctors", Keys.ENTER);
+        Thread.sleep(2000);
+        driver.navigate().back();
+    }
+    public void searchLocation(WebDriver driver)throws InterruptedException{
+        Thread.sleep(2000);
+        getSearchBtn().click();
+        getSearchInput().clear();
+        getSearchInput().sendKeys("Locations", Keys.ENTER);
+        Thread.sleep(2000);
+        driver.navigate().back();
+    }
 
 }
--- Fidelis/src/test/java/home/page/model/TestHomePage.java
+++ Fidelis/src/test/java/home/page/model/TestHomePage.java
@@ -6,11 +6,16 @@ import org.testng.annotations.Test;
 import home.page.model.HomePage;
 
 public class TestHomePage extends HomePage {
-    @Test
-    public void search(){
+    @Test// test 1
+    public void search()throws InterruptedException{
         HomePage pp =PageFactory.initElements(driver,HomePage.class);
         pp.getSearchInput();
         pp.getSearchBtn();
+        pp.clickPlan(driver);
+        pp.searchDoctors(driver);
+        pp.searchLocation(driver);
+
     }
+
 }
 
