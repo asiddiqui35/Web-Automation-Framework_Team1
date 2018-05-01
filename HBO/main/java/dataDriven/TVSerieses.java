5953163e38f966cf0ca2d988e3eb92685c66571d
ASiddiqui <aftabsiddiqui3@gmail.com>
2018-05-01 19:13:46 -0400

    Apachi POI related test case

--- HBO/main/java/dataDriven/TVSerieses.java
+++ HBO/main/java/dataDriven/TVSerieses.java
@@ -1,30 +1,43 @@
 package dataDriven;
 import base.CommonAPI;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import reporting.TestLogger;
 import utility.DataReader;
 import java.io.IOException;
 
 public class TVSerieses extends CommonAPI {
 
+    @FindBy(xpath= "//input[@placeholder='Search'and@class='components/Search--searchInput']")
+    public  WebElement searchInpuField;
 
-    @FindBy(xpath = "//input[@placeholder='Search'and@class='components/Search--searchInput']")
-    public WebElement searchInpuField;
-    public WebElement getSearchInpuField() {
+    public  WebElement getSearchInpuField() {
         return searchInpuField;
     }
+
     public void setSearchInpuField(WebElement searchInpuField) {
         this.searchInpuField = searchInpuField;
     }
-   static DataReader dr = new DataReader();
 
-    public static String[] getDataFromExcelFile() throws IOException {
-        String path = System.getProperty("user.dir") + "/data/file.xls";
-        String[] data = dr.fileReader2(path, 0);
-        return data;
-    }
     public void clickSearchIcon() {
         clickByXpath("//div[@class='bands/MainNavigation--searchIcon'][1]");
+    }
+
+    public void searchFor(String value) {
+        getSearchInpuField().sendKeys(value, Keys.ENTER);
+    }
+
+    public void searchTVShowsEnter() throws IOException {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        ReadFiles rd =new ReadFiles();
+        String[] value = rd.getDataFromExcelFile();
+        for (int i = 0; i < value.length; i++) {
+            searchFor(value[i]);
+            clickSearchIcon();
+        }
     }
 }
 
