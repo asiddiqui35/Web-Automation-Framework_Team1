3ecdaa617c5d64f0ea93d5624a42da4c606a4452
Kaniz Ruma <kaniz.ruma2012@gmail.com>
2018-05-05 10:14:21 -0400

    kaniz commit

---
+++ Amazon/src/test/java/page/object/model/TestJDBC.java
@@ -0,0 +1,19 @@
+package page.object.model;
+
+import org.openqa.selenium.support.PageFactory;
+import page.object.model.JDBC;
+import org.testng.annotations.Test;
+
+public class TestJDBC extends JDBC {
+    @Test
+    public void chekConnetion() throws Exception {
+        JDBC jdbc = PageFactory.initElements(driver, JDBC.class);
+        jdbc.readingMysqlTable();
+    }
+}
+
+
+
+
+
+
