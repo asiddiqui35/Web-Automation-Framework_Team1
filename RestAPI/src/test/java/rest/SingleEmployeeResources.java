d37ad33438d2dade535c20c41c0ffa9555e48ff0
ASiddiqui <aftabsiddiqui3@gmail.com>
2018-05-14 16:04:58 -0400

    Post google api Merge Conflict resolved

--- RestAPI/src/test/java/rest/SingleEmployeeResources.java
+++ RestAPI/src/test/java/rest/SingleEmployeeResources.java
@@ -6,13 +6,13 @@ import org.testng.annotations.Test;
 import javax.xml.ws.Response;
 
 public class SingleEmployeeResources {
-    @Test
-    public void callSingleEmployeeResources(){
-        Response response = given().when().get("http://info.venturepulse.org:8080/service-webapp/api/SingleEmployeeResources/590a4acd35522970c7956cdf").then().statusCode(200).extract().response();
-        String statusLine = response.getStatusLine();
-        int statusCode = response.getStatusCode();
-        String body = response.getBody().prettyPrint();
-        Assert.assertEquals(statusLine, "HTTP/1.1 200 OK");
-        Assert.assertEquals(statusCode, 200);
-    }
+//    @Test
+//    public void callSingleEmployeeResources(){
+//        Response response = given().when().get("http://info.venturepulse.org:8080/service-webapp/api/SingleEmployeeResources/590a4acd35522970c7956cdf").then().statusCode(200).extract().response();
+//        String statusLine = response.getStatusLine();
+//        int statusCode = response.getStatusCode();
+//        String body = response.getBody().prettyPrint();
+//        Assert.assertEquals(statusLine, "HTTP/1.1 200 OK");
+//        Assert.assertEquals(statusCode, 200);
+//    }
 }
